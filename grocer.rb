6c465383a5f80576dd48ae71dc59838afb478c4c
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-10-02 01:55:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,13 +13,18 @@ cart.each do |element_hash|
   element_name = element_hash.keys[0]
   element_stats = element_hash.values[0]
   
-  if final_hash|element_hash|
+  if final_hash.has_key?(element_name)
     element_stats[:count] += 1
   else
-    final_hash[element_name] = element_stats
-    final_hash[:count] = 1 
+    final_hash[element_name] = {
+      count: 1,
+      price: element_stats[:price],
+      clearance: element_stats[:clearance]
+      
+    }
 end
-end 
+end
+final_hash
 end
 
 
