587b52672b103f660107bf022c156621d9083026
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-10-02 02:52:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,6 +77,7 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discouunt = apply_clearance(applied_coupons)
   total - applied_discount.reduce(0) { |cart, (key, value) | cart += value[:price] * value[:count] }
+  total > 100 ? total * 0.9 : total 
   
   
 end
